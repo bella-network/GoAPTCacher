4894777f37f720442cf365aba82ced98a7a57acf
Thomas Bella <thomas@bella.network>
2025-02-01 18:38:06 +0000

    feat: Enhance server override handling by supporting path in Ubuntu and Debian server configurations

--- cmd/goaptcacher/overrides.go
+++ cmd/goaptcacher/overrides.go
@@ -19,21 +19,41 @@ func checkOverrides(r *http.Request) {
 
 	// Check if Ubuntu Server override is set
 	if config.Overrides.UbuntuServer != "" {
+		// The override destination may contain a path, so we need to split it
+		overrideParts := strings.Split(config.Overrides.UbuntuServer, "/")
+		overrideHost := overrideParts[0]
+		overridePath := strings.Join(overrideParts[1:], "/")
+
 		// If destination host is *.archive.ubuntu.com or archive.ubuntu.com, remap to the configured server
-		if strings.HasSuffix(r.Host, "archive.ubuntu.com") || strings.HasSuffix(r.Host, ".archive.ubuntu.com") && r.Host != config.Overrides.UbuntuServer {
+		if strings.HasSuffix(r.Host, "archive.ubuntu.com") || strings.HasSuffix(r.Host, ".archive.ubuntu.com") && r.Host != overrideHost {
 			log.Printf("[INFO:OVERRIDE:UBUNTU] Overriding %s to %s\n", r.Host, config.Overrides.UbuntuServer)
-			r.Host = config.Overrides.UbuntuServer
-			r.URL.Host = config.Overrides.UbuntuServer
+			r.Host = overrideHost
+			r.URL.Host = overrideHost
+
+			// If the override path is set, append it to the request URL
+			if overridePath != "" {
+				r.URL.Path = overridePath + r.URL.Path
+			}
 		}
 	}
 
 	// Check if Debian Server override is set
 	if config.Overrides.DebianServer != "" {
+		// The override destination may contain a path, so we need to split it
+		overrideParts := strings.Split(config.Overrides.DebianServer, "/")
+		overrideHost := overrideParts[0]
+		overridePath := strings.Join(overrideParts[1:], "/")
+
 		// If destination host is ftp.{country}.debian.org, remap to the configured server
-		if (strings.HasSuffix(r.Host, "debian.org") && strings.HasPrefix(r.Host, "ftp.") || r.Host == "deb.debian.org") && r.Host != config.Overrides.DebianServer {
+		if (strings.HasSuffix(r.Host, "debian.org") && strings.HasPrefix(r.Host, "ftp.") || r.Host == "deb.debian.org") && r.Host != overrideHost {
 			log.Printf("[INFO:OVERRIDE:DEBIAN] Overriding %s to %s\n", r.Host, config.Overrides.DebianServer)
-			r.Host = config.Overrides.DebianServer
-			r.URL.Host = config.Overrides.DebianServer
+			r.Host = overrideHost
+			r.URL.Host = overrideHost
+
+			// If the override path is set, append it to the request URL
+			if overridePath != "" {
+				r.URL.Path = overridePath + r.URL.Path
+			}
 		}
 	}
 }
