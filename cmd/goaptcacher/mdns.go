659798157d9c79e97741294a0510dd46dd60d386
Thomas Bella <thomas@bella.network>
2026-02-15 21:02:31 +0000

    fix: Correct service name format in mDNS registration. Fixes #4

--- cmd/goaptcacher/mdns.go
+++ cmd/goaptcacher/mdns.go
@@ -8,7 +8,7 @@ import (
 
 func mDNSAnnouncement() {
 	// Create a service entry
-	_, err := zeroconf.Register("GoAPTCacher", "_apt_proxy._tcp", "local.", config.ListenPort, nil, nil)
+	_, err := zeroconf.Register("_apt_proxy", "_tcp", "local.", config.ListenPort, nil, nil)
 	if err != nil {
 		log.Println("[ERR:mDNS] Failed to register service: ", err)
 		return
